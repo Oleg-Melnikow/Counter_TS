906879a09a703cd102d79d3d9d007c22abb99006
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-03-18 16:04:05 +0300

    add Counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React, {useState} from 'react';
 import './App.css';
-import {InfoBoard} from "./components/InfoBoard";
-import {Button} from "./components/Button";
+import {Counter} from "./components/Counter";
 
 function App() {
     let [count, setCount] = useState<number>(0);
     const limitValue = 5;
-
-    const incrementHandler = () => setCount(++count);
     let equal = (limitValue === count);
+    const incrementHandler = () => setCount(++count);
     const resetHandler = () => setCount(0);
 
     return (
         <div className="App">
-            <div className="wrap-counter">
-                <InfoBoard count={count} isLimitValue={equal}/>
-                <div className="wrap-button">
-                    <Button name="increment" onClickHandler={incrementHandler} disabled={equal}/>
-                    <Button name="reset" onClickHandler={resetHandler} disabled={!count}/>
-                </div>
-            </div>
+            <Counter count={count} equal={equal} incrementHandler={incrementHandler} resetHandler={resetHandler}/>
         </div>
     )
 }
